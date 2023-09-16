ea55364a64c437a89a2146c6b48121632896e1bc
Aaron <squishyguy10@Gmail.com>
2023-09-16 15:31:59 -0400

    edited camera

--- testing-smooth.py
+++ testing-smooth.py
@@ -18,7 +18,7 @@ smooth = 7
 # Calculate destination points to match the input image size
 pts2 = np.float32([[0, 0], [1920, 0], [0, 1080], [1920, 1080]])
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(3, 1920)
 cap.set(4, 1080)
 cap.set(15, -4)
