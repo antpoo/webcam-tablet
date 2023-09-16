3eac892f70ec0b337242348b45fce2d85f10bc69
PenguinPush <icyzminecraft@gmail.com>
2023-09-16 17:30:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -56,11 +56,16 @@ def selectFourPoints():
 
     return True
 
+def calcPerspective(p, M):
+    xPos_m = (M[0][0] * p[0] + M[0][1] * p[1] + M[0][2]) / ((M[2][0] * p[0] + M[2][1] * p[1] + M[2][2]))
+    yPos_m = (M[1][0] * p[0] + M[1][1] * p[1] + M[1][2]) / ((M[2][0] * p[0] + M[2][1] * p[1] + M[2][2]))
+    return (xPos_m, yPos_m)
 
 cv2.namedWindow('Select Corners')
 cv2.setMouseCallback('Select Corners', draw_circle)
 
 click_threshold = 0
+hover_threshold = 0
 
 while True:
     if (selectFourPoints()):
@@ -111,6 +116,11 @@ while True:
                         for b in range(4):
                             image = cv2.line(image, pts[a], pts[b], (255, 0, 255), 8)
 
+                    # tl_x, tl_y = calcPerspective(pts[3], M)
+                    # bl_x, bl_y = calcPerspective(pts[1], M)
+                    # tr_x, tr_y = calcPerspective(pts[2], M)
+                    # br_x, br_y = calcPerspective(pts[0], M)
+
                     tl_x, tl_y = pts[3]
                     bl_x, bl_y = pts[1]
                     tr_x, tr_y = pts[2]
@@ -121,17 +131,16 @@ while True:
                     paper_right_pos = max(tr_x, br_x)
                     paper_bottom_pos = max(bl_y, br_y)
 
-                    # p = [handLms.landmark[8].x * 1920, handLms.landmark[8].y * 1000]  # your original point
-                    # xPos_m = (M[0][0] * p[0] + M[0][1] * p[1] + M[0][2]) / ((M[2][0] * p[0] + M[2][1] * p[1] + M[2][2]))
-                    # yPos_m = (M[1][0] * p[0] + M[1][1] * p[1] + M[1][2]) / ((M[2][0] * p[0] + M[2][1] * p[1] + M[2][2]))
+                    # xPos_m, yPos_m = calcPerspective([handLms.landmark[8].x * 1920, handLms.landmark[8].y * 1000], M)
+
                     xPos_m = handLms.landmark[8].x * 1920
                     yPos_m = handLms.landmark[8].y * 1080
                     zPos_m = handLms.landmark[8].z
-                    #
+                    
                     # xPos = (xPos_m - paper_right_pos) / (paper_left_pos - paper_right_pos)
                     # yPos = (yPos_m - paper_bottom_pos) / (paper_top_pos - paper_bottom_pos)
 
-                    if paper_left_pos <= xPos_m <= paper_right_pos and paper_top_pos <= yPos_m <= paper_bottom_pos:
+                    if paper_left_pos <= xPos_m <= paper_right_pos and paper_top_pos <= yPos_m <= paper_bottom_pos: ## and zPos_m <= hover_threshold and hover_threshold != 0.0:
                         # xPos = (paper_right_pos - xPos_m) / paper_left_pos * 1920
                         # yPos = (paper_bottom_pos - yPos_m) / paper_top_pos * 1080
                         xPos = (xPos_m - paper_right_pos) / (paper_left_pos - paper_right_pos)
@@ -140,20 +149,22 @@ while True:
                         print(xPos)
                         print(yPos)
 
-                        if zPos_m <= click_threshold and click_threshold != 0.0:
+                        if zPos_m >= click_threshold and click_threshold != 0.0:
                             mouse.press()
                             print('click')
                         else:
                             mouse.release()
 
                         mouse.move(xPos * 1920, yPos * 1080, True)
+                    else:
+                        mouse.release()
                     try:
                         image = cv2.putText(image, str(xPos) + " " + str(yPos), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
                                             (255, 0, 225), 2, cv2.LINE_AA)
                     except:
                         pass
-                    image = cv2.putText(image, str(xPos_m) + " " + str(yPos_m), (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                                        (255, 0, 225), 2, cv2.LINE_AA)
+                    image = cv2.putText(image, str(xPos_m) + " " + str(yPos_m), (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 225), 2, cv2.LINE_AA)
+                    image = cv2.putText(image, str(zPos_m) + " " + str(click_threshold), (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 225), 2, cv2.LINE_AA)
 
             cv2.imshow('Perspective Transformation', image)
             key = cv2.waitKey(1)
@@ -163,6 +174,9 @@ while True:
                 break
             elif key == ord(' '):
                 click_threshold = zPos_m
+            elif key == ord('1'):
+                hover_threshold = zPos_m
+                print("Hover Threshold:", hover_threshold)
 
             # print(pts)
 
